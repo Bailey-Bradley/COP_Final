38c5630dcebad3e58f6fd22991ecbf001de65d80
Bailey <bb25@usf.edu>
2025-11-21 17:46:59 -0500

    Implemented addEdge() and addVertex()

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,6 +1,43 @@
+#include <stdexcept>
+
 #include "Graph.h"
+#include "Vertex.h"
+#include "Edge.h"
 
 class PriorityQueue {}; // THIS IS JUST TO MAKE INTELLISENSE SHUT UP WHILE WE DONT HAVE A PRIORTIY QUEUE
+
+void Graph::addVertex(std::string label) {
+    vertices.try_emplace(label, Vertex(label));
+}
+
+void Graph::addEdge(std::string label1, std::string label2, unsigned long weight) {
+    try {
+
+        vertices.at(label1);
+        vertices.at(label2);
+
+    } catch (const std::out_of_range& e) {
+        return;
+    }
+
+    Vertex& vertex1 = vertices[label1];
+    Vertex& vertex2 = vertices[label2];
+
+    if (vertex1.isAdjacentTo(vertex2)) return;
+
+    Edge* newEdge = new Edge(&vertex1, &vertex2, weight);
+
+    vertex1.addEdge(newEdge);
+    vertex2.addEdge(newEdge);
+}
+
+void Graph::removeVertex(std::string label) {
+
+}
+
+void Graph::removeEdge(std::string label1, std::string label2) {
+
+}
 
 unsigned long Graph::shortestPath(std::string startLabel, std::vector<std::string> &path) {
     PriorityQueue pq;
