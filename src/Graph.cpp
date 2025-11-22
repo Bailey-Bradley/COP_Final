2269ff848247e10019cc9f54a6f0ca34c61de16a
Bailey <bb25@usf.edu>
2025-11-22 15:45:49 -0500

    Implemented Dijkstra's

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -53,23 +53,88 @@ void Graph::removeEdge(std::string label1, std::string label2) {
         vertex2.removeEdge(edge);
     }
 }
+struct VertexPathNode {
+    Vertex *previous_vertex;
+    Vertex *vertex;
+    unsigned long cost;
+};
 
-bool compareVertices(std::pair<Vertex *, int> v1, std::pair<Vertex *, int> v2) {
-    return v1.second < v2.second;
+bool compareVertices(VertexPathNode v1, VertexPathNode v2) {
+    return v1.cost < v2.cost;
+}
+
+void vertexPathToStrings(std::vector<std::string>& string_array, std::vector<VertexPathNode>& vertex_paths, const std::string& end_vertex_label) {
+
+    VertexPathNode* node = nullptr;
+
+    for (VertexPathNode& path_node : vertex_paths) {
+        if (path_node.vertex->getLabel() == end_vertex_label) {
+            node = &path_node; 
+        }
+    }
+
+    while (node->previous_vertex != nullptr) {
+        string_array.push_back(node->vertex->getLabel());
+
+        for (VertexPathNode& path_node : vertex_paths) {
+            if (path_node.vertex == node->previous_vertex) {
+                node = &path_node; 
+            }
+        }
+    }
+
+    std::reverse(string_array.begin(), string_array.end());
 }
 
 unsigned long Graph::shortestPath(std::string startLabel, std::string endLabel, std::vector<std::string> &path) {
-    std::vector<Vertex*> processed;
 
-    PriorityQueue<std::pair<Vertex*, int>, compareVertices> vertexQueue;
+    std::vector<VertexPathNode> processed;
+
+    PriorityQueue<VertexPathNode, compareVertices> vertexQueue;
 
     try {
         vertices.at(startLabel);
+        vertices.at(endLabel);
     } catch (const std::out_of_range &e) {
         return -1;
     }
 
-    Vertex &startVertex = vertices[startLabel];
+    Vertex *startVertex = &vertices[startLabel];
+    vertexQueue.enqueue(VertexPathNode({nullptr, startVertex, 0}));
+
+    Vertex *endVertex = &vertices[endLabel];
+
+    while (vertexQueue.size() > 0) {
+        VertexPathNode& vertex_path_node = vertexQueue.min();
+
+        Vertex *current_vertex = vertex_path_node.vertex;
+        unsigned long current_vertex_cost = vertex_path_node.cost;
+
+        for (Edge *edge : current_vertex->getEdges().getQueue()) {
+            Vertex *adjacent = edge->getOpposite(current_vertex);
+
+            bool shouldSkip = false;
+            for (VertexPathNode& processed_pair : processed) {
+                if (processed_pair.vertex == adjacent) {
+                    shouldSkip = true;
+                    break;
+                }
+            }
+
+            if (shouldSkip)
+                continue;
+
+            vertexQueue.enqueue(VertexPathNode({current_vertex, adjacent, current_vertex_cost + edge->weight}));
+        }
+
+        processed.push_back(vertex_path_node);
+        vertexQueue.dequeue();
+
+        if (current_vertex == endVertex) {
+            vertexPathToStrings(path, processed, endLabel);
+            return current_vertex_cost;
+        }
+    }
 
     return -1;
 }
