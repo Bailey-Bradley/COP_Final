219e4da781a32a99afe1ec741170e12ffd7fdc33
Bailey <bb25@usf.edu>
2025-11-21 21:46:40 -0500

    Implemented getOpposite() and isEqual()

--- src/Edge.cpp
+++ src/Edge.cpp
@@ -1,4 +1,5 @@
 #include "Edge.h"
+#include "Vertex.h"
 
 bool Edge::Comparator::operator()(const Edge *const edge1, const Edge *const edge2) const {
     return edge1->weight < edge2->weight;
@@ -6,4 +7,36 @@ bool Edge::Comparator::operator()(const Edge *const edge1, const Edge *const edg
 
 bool Edge::Comparator::operator()(const Edge &edge1, const Edge &edge2) const {
     return edge1.weight < edge2.weight;
+}
+
+Vertex* Edge::getOpposite(const Vertex* const vertex) {
+    if (vertex == vertex1) {
+        return vertex2;
+    } else if (vertex == vertex2) {
+        return vertex1;
+    } else {
+        return nullptr;
+    }
+}
+
+bool Edge::isEqual(const Edge &edge) {
+    if (edge.vertex1 == vertex1 && edge.vertex2 == vertex2 && edge.weight == weight) {
+        return true;
+    } else if (edge.vertex1 == vertex2 && edge.vertex2 == vertex1 && edge.weight == weight) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool Edge::isEqual(const Edge *const edge) {
+    return isEqual(*edge);
+}
+
+bool Edge::isIncidentOn(const Vertex &vertex) {
+
+}
+
+bool Edge::isIncidentOn(const Vertex *const vertex) {
+
 }
