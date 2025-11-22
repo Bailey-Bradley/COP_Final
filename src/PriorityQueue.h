9d5b52150d82a812373f862d41a9a5369df0fcae
Bailey <bb25@usf.edu>
2025-11-22 12:51:05 -0500

    Fixed comparator type in template argument

--- src/PriorityQueue.h
+++ src/PriorityQueue.h
@@ -1,11 +1,12 @@
+#pragma once
+
 #include <algorithm>
 #include <list>
 
-template<typename T, bool (&isLess)(const T&, const T&)>
+template<typename T, bool (&isLess)(T, T)>
 class PriorityQueue {
 
     std::list<T> queue;
-    C isLess;
 
 public:
 
@@ -19,32 +20,32 @@ public:
     const std::list<T>& getQueue();
 };
 
-template<typename T, bool (&isLess)(const T&, const T&)>
+template<typename T, bool (&isLess)(T, T)>
 void PriorityQueue<T, isLess>::enqueue(T newItem) {
     queue.push_back(newItem);
 }
 
-template<typename T, bool (&isLess)(const T&, const T&)>
+template<typename T, bool (&isLess)(T, T)>
 T PriorityQueue<T, isLess>::min() {
     auto lowest_priority = std::min_element(queue.begin(), queue.end(), isLess);
 
     return *lowest_priority;
 }
 
-template<typename T, bool (&isLess)(const T&, const T&)>
+template<typename T, bool (&isLess)(T, T)>
 T PriorityQueue<T, isLess>::dequeue() {
     T lowest_priority = min();
     queue.remove(lowest_priority);
 
-    return *lowest_priority;
+    return lowest_priority;
 }
 
-template<typename T, bool (&isLess)(const T&, const T&)>
+template<typename T, bool (&isLess)(T, T)>
 const std::list<T>& PriorityQueue<T, isLess>::getQueue() {
     return queue;
 }
 
-template<typename T, bool (&isLess)(const T&, const T&)>
+template<typename T, bool (&isLess)(T, T)>
 void PriorityQueue<T, isLess>::remove(T element) {
     queue.remove(element);
 }
