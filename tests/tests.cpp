0b215ac3eeec529ca93c91f13d347da1f9ae6abd
quandomonkey <merpmasta@gmail.com>
2025-11-22 21:56:29 -0500

    First couple test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,13 +4,27 @@
 TEST_CASE("Testing the testing") { // Tests if catch is functioning properly
     REQUIRE( 1 == 1);
 }
-// Testing if addition functions correctly
-TEST_CASE("Add Vertices") {
-    Graph g; // Identifying our graph
-    // Choosing 2 vertices to sum together
-    g.addVertex("A");
-    g.addVertex("B");
-    std::vector<std::string> path; // Setting up vector
 
-    REQUIRE(g.shortestPath("A","B", path) == (unsigned long)-1);   
+TEST_CASE("Final Project Test Cases for Dijkstra's Algorithm") {
+    Graph g;
+
+    SECTION("Should return infinity for non-connected vertices") {
+        g.addVertex("A");
+        g.addVertex("B");
+        std::vector<std::string> path;
+
+        REQUIRE(g.shortestPath("A","B", path) == (unsigned long)-1);
+    }
+
+    SECTION("Should return shortest path without getting stuck in a cycle") {
+        g.addVertex("A");
+        g.addVertex("B");
+        g.addVertex("C");
+        g.addEdge("A","B",1);
+        g.addEdge("B","C",1);
+        g.addEdge("C","A",1);
+        std::vector<std::string> path;
+
+        REQUIRE(g.shortestPath("A","C", path) == 2);
+    }
 }
