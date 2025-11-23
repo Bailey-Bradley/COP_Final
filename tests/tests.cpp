7c257b5c3fef00b31366012dfff2d444df36a83d
Broshi21 <undyingcash@gmail.com>
2025-11-22 20:12:51 -0500

    hello

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,3 +3,14 @@
 TEST_CASE("Testing the testing") { // Tests if catch is functioning properly
     REQUIRE( 1 == 1);
 }
+// Testing if addition functions correctly
+TEST_CASE("Add Vertices") {
+    Graph g; // Identifying our graph
+    // Choosing 2 vertices to sum together
+    g.addVertex("A");
+    g.addVertex("B");
+    std::vector<std::string> path; // Setting up vector
+
+    REQUIRE(g.shortestPath("A","B", path) == (unsigned long)-1);
+    )
+}
