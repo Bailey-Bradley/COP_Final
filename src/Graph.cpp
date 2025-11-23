dd2fa3a58f5bdef396f7ff3ec558e9d4c165d95c
Broshi21 <undyingcash@gmail.com>
2025-11-22 23:29:19 -0500

    Added comments to graph.cpp

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,4 +1,10 @@
-// Christian McMillen
+/*
+Graph.cpp:
+
+This implementation represents the Graph class
+
+Adds/removes vertices and edges, finds shortest path, applies Dijkstra's Algorithm, and returns the total cost of the chosen path
+*/
 #include <stdexcept>
 
 #include "Edge.h"
@@ -6,39 +12,45 @@
 #include "PriorityQueue.h"
 #include "Vertex.h"
 
+// Adds a new vertex to the graph.
 void Graph::addVertex(std::string label) {
-    vertices.try_emplace(label, Vertex(label));
+    vertices.try_emplace(label, Vertex(label)); // try_emplace avoids duplicate vertices
 }
 
+// Adds an edge between two vertices identified by the labels
 void Graph::addEdge(std::string label1, std::string label2,
                     unsigned long weight) {
     try {
-
+        // Checks if vertices exist
         vertices.at(label1);
         vertices.at(label2);
 
+    // Returns if either vertex does not exist or the edge already exists
     } catch (const std::out_of_range &e) {
         return;
     }
-
     Vertex &vertex1 = vertices[label1];
     Vertex &vertex2 = vertices[label2];
 
     if (vertex1.isAdjacentTo(vertex2))
         return;
 
+    // Creates new edge to add to vertices
     Edge *newEdge = new Edge(&vertex1, &vertex2, weight);
 
     vertex1.addEdge(newEdge);
     vertex2.addEdge(newEdge);
 }
 
+// Removes the identified vertex and connected edges from the graph
 void Graph::removeVertex(std::string label) { vertices.erase(label); }
 
+// Removes edge between identifed vertices
 void Graph::removeEdge(std::string label1, std::string label2) {
     try {
         vertices.at(label1);
         vertices.at(label2);
+    // If the vertex or the edge does not exist just return
     } catch (const std::out_of_range &e) {
         return;
     }
@@ -46,6 +58,7 @@ void Graph::removeEdge(std::string label1, std::string label2) {
     Vertex &vertex1 = vertices[label1];
     Vertex &vertex2 = vertices[label2];
 
+    // Obtain edge and remove it from vertices
     if (vertex1.isAdjacentTo(vertex2)) {
         Edge *edge = vertex1.getEdge(vertex2);
 
@@ -53,16 +66,18 @@ void Graph::removeEdge(std::string label1, std::string label2) {
         vertex2.removeEdge(edge);
     }
 }
+// This struct tracks vertices and their paths through travel calculation
 struct VertexPathNode {
-    Vertex *previous_vertex;
-    Vertex *vertex;
-    unsigned long cost;
+    Vertex *previous_vertex; // Pointer to path vertex
+    Vertex *vertex; // Current
+    unsigned long cost; // Cost so far
 
     bool operator==(const VertexPathNode& vpn) const {
         return (vpn.previous_vertex == previous_vertex) && (vpn.vertex == vertex) && (vpn.cost == cost);
     }
 };
 
+// Comparator for ordering VertexPathNodes by cost
 bool compareVertices(VertexPathNode v1, VertexPathNode v2) {
     return v1.cost < v2.cost;
 }
@@ -96,11 +111,12 @@ unsigned long Graph::shortestPath(std::string startLabel, std::string endLabel, 
 
     PriorityQueue<VertexPathNode, compareVertices> vertexQueue;
 
+    // Ensure that there are start and end vertices
     try {
         vertices.at(startLabel);
         vertices.at(endLabel);
     } catch (const std::out_of_range &e) {
-        return -1;
+        return -1; // Return when at least one vertice not found
     }
 
     Vertex *startVertex = &vertices[startLabel];
@@ -108,37 +124,43 @@ unsigned long Graph::shortestPath(std::string startLabel, std::string endLabel, 
 
     Vertex *endVertex = &vertices[endLabel];
 
+    // Loop through vertices in cost order
     while (vertexQueue.size() > 0) {
         VertexPathNode& vertex_path_node = vertexQueue.min();
 
         Vertex *current_vertex = vertex_path_node.vertex;
         unsigned long current_vertex_cost = vertex_path_node.cost;
 
+        // Navigate through every edge adjacent to the current vertex
         for (Edge *edge : current_vertex->getEdges().getQueue()) {
             Vertex *adjacent = edge->getOpposite(current_vertex);
 
+            // Identify if adjacent vertex has already been visited
             bool shouldSkip = false;
             for (VertexPathNode& processed_pair : processed) {
                 if (processed_pair.vertex == adjacent) {
-                    shouldSkip = true;
+                    shouldSkip = true; // If it has then skip to avoid repeats
                     break;
                 }
             }
 
             if (shouldSkip)
                 continue;
-
+            // Take priority queue and add the adjacent vertex with the new cost
             vertexQueue.enqueue(VertexPathNode({current_vertex, adjacent, current_vertex_cost + edge->weight}));
         }
 
+        // Identify current vertex
         processed.push_back(vertex_path_node);
         vertexQueue.dequeue();
 
+        // Upon reaching ending vertex reconstruct the desired path
         if (current_vertex == endVertex) {
             vertexPathToStrings(path, processed, endLabel);
-            return current_vertex_cost;
+            return current_vertex_cost; // Return total cost
         }
     }
 
+    // Return in case of no path being found
     return -1;
 }
