4cd69fa52d9b5537a89bb93d83a1d6b423a0234a
Broshi21 <undyingcash@gmail.com>
2025-11-23 19:19:23 -0500

    added testcase for the main.cpp file, please double check it if you have the time

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <vector>
+#include "Graph.hpp"
+
 // Tests that setup functions as intended
 int main(int, char**){
     std::cout << "Test\n";
+
+    // Testing shortest path cost
+    Graph g;
+    std::vector<std::string> path;
+
+    g.addVertex("A");
+    g.addVertex("B");
+    g.addVertex("C");
+    g.addEdge("A","B", 8); // Path cost from A to B
+    g.addEdge("B","C", 2); // Path cost from B to C
+
+    unsigned long cost = g.shortestPath("A", "C", path);
+    std::cout << "Path from A to C costs: " << cost << std::endl;
+
+    // Display path to user
+    std::cout << "Chosen path: ";
+    for (const std::string& vertex : path) {
+        std::cout << vertex << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
